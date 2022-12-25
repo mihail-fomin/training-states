40f6fe9ab6e413e93da56e2b2fe89700d4a86098
misha133kv <mihail_fomin_95@mail.ru>
2022-12-25 16:55:38 +0500

    added class

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,47 @@
+import React, { ReactDOM, Component } from "react"
+
 function Button(props) {
 	return (
 		<div className='container mx-auto '>
-			<button className="w-full p-2 text-xl text-left border-2 border-gray-400 rounded hover:bg-slate-200" >
+			<button className="non-active" >
 				{props.number} button
 			</button>
 		</div>
 	)
 }
 
-export default function Home() {
-	return (
-		<div>
-			<Button
-				number={'First'} />
-			<Button
-				number={'Second'} />
-			<Button
-				number={'Third'} />
-			<Button
-				number={'Fourth'} />
-			<Button
-				number={'Fifth'} />
-		</div>
-	)
+export default class Component extends Component() {
+	constructor(props) {
+		super(props);
+		this.state = { isToggleOn: true };
+		this.handleClick = this.handleClick.bind(this);
+
+		handleClick() {
+			this.setState(prevState => ({
+				isToggleOn: !prevState.isToggleOn
+			}));
+		}
+
+		render() {
+			return (
+				<div>
+					<Button
+						onClick={this.handleClick}
+						number={'First'} />
+					<Button
+						onClick={this.handleClick}
+						number={'Second'} />
+					<Button
+						onClick={this.handleClick}
+						number={'Third'} />
+					<Button
+						onClick={this.handleClick}
+						number={'Fourth'} />
+					<Button
+						onClick={this.handleClick}
+						number={'Fifth'} />
+				</div>
+			);
+		}
+	}
 }
