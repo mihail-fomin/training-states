802115c112e381778d7ce25c8bb1dfbe4aefbd63
misha133kv <mihail_fomin_95@mail.ru>
2023-01-12 20:16:58 +0500

    deleted async function

--- pages/3.js
+++ pages/3.js
@@ -2,63 +2,47 @@ import * as React from 'react'
 import { MainMenu } from '../components/MainMenu'
 import Container from '../components/Container'
 
+const lettersMaxNum = 140;
+
 export default function Form() {
 	const [tweet, setTweet] = React.useState('');
-	const [status, setStatus] = React.useState('typing');
-	const [counter, setCounter] = React.useState(140);
+	const [counter, setCounter] = React.useState(lettersMaxNum);
+
 
 	function handleTextAreaChange(e) {
 		setTweet(e.target.value);
+		setCounter(n => n - 1)
 	}
 
-	async function handleSubmit(e) {
+	function handleSubmit(e) {
 		e.preventDefault();
-		setStatus('submitting');
-		try {
-			await submitForm(tweet);
-			setStatus('success');
-			return alert('Your tweet has been sent!')
-		} catch (err) {
-			setStatus('typing');
-		}
+		submitForm(tweet);
+		return alert('Your tweet has been sent!')
 	}
 
 	return <div>
 		<Container>
 			<MainMenu />
-			<h1 className='my-4 text-xl'>Tweet</h1>
-			<form onSubmit={handleSubmit}>
-				<textarea
-					className='p-2 border-2 rounded h-36 w-72 border-sky-600'
-					type='textarea'
-					placeholder='Input up to 140 characters...'
-					onChange={handleTextAreaChange}
-					disabled={status === 'submitting'}
-				>
-				</textarea>
-				<div className='flex items-center'>
-					<button
-						className='block p-2 text-white rounded cursor-pointer bg-sky-600'
-						disabled={
-							tweet.length === 0 ||
-							status === 'submitting'
-						}>
-						Tweet
-					</button>
-					<div className='pl-40'>{counter}</div>
-				</div>
-			</form>
+			<div className='mx-auto w-[300px]'>
+				<h1 className='my-4 text-xl'>Tweet</h1>
+				<form onSubmit={handleSubmit}>
+					<textarea
+						className='w-full p-2 border-2 rounded h-36 border-sky-600'
+						value={tweet}
+						placeholder='Input up to 140 characters...'
+						onChange={handleTextAreaChange}
+					>
+					</textarea>
+					<div className='flex items-center justify-between'>
+						<button
+							className='block p-2 text-white rounded cursor-pointer bg-sky-600'
+							disabled={tweet.length === 0}>
+							Tweet
+						</button>
+						<div >{counter}</div>
+					</div>
+				</form>
+			</div>
 		</Container>
 	</div>
-}
-
-function submitForm(tweet) {
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			if (!tweet) { reject(new Error('Type something')) }
-			else {
-				resolve();
-			}
-		}, 1500);
-	});
 }
