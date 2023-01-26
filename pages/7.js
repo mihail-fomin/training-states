842047d0e13ea282b3ec913ca616afb97f6a8f46
misha133kv <mihail_fomin_95@mail.ru>
2023-01-26 20:10:34 +0500

    finished passqword generator

--- pages/7.js
+++ pages/7.js
@@ -3,22 +3,39 @@ import { MainMenu } from '../components/MainMenu'
 import Container from '../components/Container'
 
 export default function () {
-	const [length, setLength] = React.useState(6)
 	const [value, setValue] = React.useState('')
+	const [length, setLength] = React.useState(6)
+	const [lowercase, setLowercase] = React.useState(false)
+	const [uppercase, setUppercase] = React.useState(false)
+	const [symbol, setSymbol] = React.useState(false)
+
+
 	function onLengthChange(event) {
 		setLength(event.target.value);
 	}
 
 	function onGenerateButtonClick(event) {
 		event.preventDefault()
-		let chrs = 'abdehkmnpswxzABDEFGHKMNPQRSTWXZ123456789';
 		let str = ''
+		let numbers = '0123456789'
+		let lwrcase = 'abdehkmnpswxz'
+		let upprcase = 'ABDEFGHKMNPQRSTWX'
+		let symbols = '-_'
+		let chrs = numbers
+
+		if (lowercase) chrs += lwrcase
+		if (uppercase) chrs += upprcase
+		if (symbol) chrs += symbols
+
+
+
 		for (let i = 0; i < length; i++) {
 			let pos = Math.floor(Math.random() * chrs.length);
 			str += chrs.substring(pos, pos + 1)
 		}
 		setValue(str)
 	}
+
 	return <>
 		<Container>
 			<MainMenu />
@@ -37,8 +54,25 @@ export default function () {
 					type='range'
 					min='6'
 					onChange={onLengthChange}
-					max='20' />
+					max='12' />
 				<label htmlFor='range'>{length}</label>
+				<div className='mt-3'>
+					<input type='checkbox' name='numbers' checked={true} />
+					<label className='mx-2'>Numbers (always enabled)</label>
+				</div>
+				<div className='my-2'>
+					<input type='checkbox' name='lowercase' onChange={() => setLowercase(!lowercase)} />
+					<label className='mx-2'>Lowercase</label>
+				</div>
+				<div className='my-2'>
+					<input type='checkbox' name='uppercase' onChange={() => setUppercase(!uppercase)} />
+					<label className='mx-2'>Uppercase</label>
+				</div>
+				<div className='my-2'>
+					<input type='checkbox' name='symbols' onChange={() => setSymbol(!symbol)} />
+					<label className='mx-2'>Symbols</label>
+				</div>
+
 				<button
 					className='block p-2 my-2 text-white rounded cursor-pointer bg-sky-600 hover:bg-sky-700'
 					onClick={onGenerateButtonClick}
